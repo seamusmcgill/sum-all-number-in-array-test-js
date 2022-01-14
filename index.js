272f36eea61a84087a7c586cdd350d97b6664237
Seamus McGill <seamusmcgill@gmail.com>
2022-01-14 18:06:57 -0500

    Created recursive function to sum nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0;
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      sum = sum + sumItems(item);
+      continue;
+    }
+    sum += item;
+  }
+  return sum;
+};
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 
 module.exports = sumItems;
